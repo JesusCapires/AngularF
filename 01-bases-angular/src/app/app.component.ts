3b3fc2e5adb5f6b7c4e1f187242cfc631a3ded47
JesusCapires <capa.2901@gmail.com>
2024-01-31 09:44:49 -0600

    Complete seccion for angular course

--- 01-bases-angular/src/app/app.component.ts
+++ 01-bases-angular/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector
-: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'curso_angular';
-  public counter: number =10;
-  increaseBY(value: string): void{
-    value == 'inc'? this.counter +=1: this.counter -=1;
-  }
+
+  public title: string = 'Hola Mundo';
+
+
 }
