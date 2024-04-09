5d6867f3378d53cebdaed286463054705c234877
JesusCapires <capa.2901@gmail.com>
2024-04-09 10:10:29 -0600

    Inicio rutas hijas y lazyLoad

---
+++ 06-heroesApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
