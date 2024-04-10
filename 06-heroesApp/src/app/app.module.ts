918ca1a8f87faa75e2b8abccd714a805e17139a3
JesusCapires <capa.2901@gmail.com>
2024-04-10 14:01:18 -0600

    Fin sección 14: Angular Material

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -12,8 +13,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     AppComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [
