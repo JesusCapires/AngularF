a3333e8e6588abdd0d5b35f6df155d5b05500d25
JesusCapires <capa.2901@gmail.com>
2024-04-11 13:37:01 -0600

    Comentarios operadores rxjs

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,4 @@
-import { filter, switchMap, tap } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
@@ -39,22 +39,35 @@ export class NewPageComponent implements OnInit{
     private dialog: MatDialog
   ) {}
 
+  //GETTERS PARA OBTENER EL VALOR ACTUAL DEL FORMULARIO
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
 
     return hero;
   }
 
+
+  //METODO QUE SE EJECUTA UNA VEZ QUE EL COMPONENTE SE HAYA CARGADO POR COMPLETO, SIMILAR A LA CARGA DEL DOM EN JS
   ngOnInit(): void {
     if(!this.router.url.includes('edit') ) return;
 
     this.activatedRoute.params
+    //TUBERIA PARA TRANSOFRMAR/MANIPULAR DATOS
       .pipe(
+        //EMITIR VALORES / DISPARAR EFECTOS SECUNDARIOS
+        tap( valores => console.log(valores) ),
+        // MANIPULA Y MODIFICA DATOS CON FACILIDAD
+        // map( ({id}) => {
+        //   console.log(id*10)
+        // } ),
+        // OBTIENE EL VALOR DE UN OBSERVABLE Y LANZA OTRO OBSERVABLE
         switchMap(({id}) => this.heroesService.getHeroById(id)),
       ).subscribe( hero => {
         if(!hero) {
+          //NAVIGATEBYURL PERMITE transportar a los usuarios a diferentes urls
           return this.router.navigateByUrl('/');
         }
+        //RESET DEVUELVE EL FORMULARIO A SU ESTADO INICIAL
         this.heroForm.reset(hero);
         return;
       })
@@ -90,6 +103,7 @@ export class NewPageComponent implements OnInit{
 
       dialogRef.afterClosed()
         .pipe(
+          //FILTRO QUE PERMITE PASAR SOLO LO QUE CUMPLE LA CONDICIÓN AGREGADA
           filter( (result: boolean) => result),
           switchMap( () => this.heroesService.deleteHero(this.currentHero.id)),
           filter((wasDeleted: boolean) => wasDeleted),
